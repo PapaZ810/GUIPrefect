6fd70490b2929b22a52ade91615f96adb76dcf99
Zac <zombiezac@att.net>
2018-12-04 13:20:07 -0700

    added a window

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -17,30 +17,38 @@ public class GUIPanel extends JPanel
 		
 		this.appController = appController;
 		
-		this.textLabel = new JLabel("this is a color apppppppppp");
-		this.colorButton = new JButton("Click to change coloooloolooolors");
+		this.textLabel = new JLabel("This is a color App"); 
+		this.colorButton = new JButton("Click to change colours");
 		
 		setupPanel();
 		setupLayout();
 		setupListeners();
-		
 	}
 	
 	private void setupPanel()
 	{
-		this.setBackground(Color.MAGENTA);
+		
 		this.add(textLabel);
 		this.add(colorButton);
 	}
 	
 	private void setupLayout()
 	{
-		
+		this.setBackground(new Color(0, 150, 150));
+		colorButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		colorButton.setBackground(new Color(105, 105, 105));
+		textLabel.setBackground(new Color(105, 105, 105));
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.NORTH, textLabel, 156, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, textLabel, 378, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.WEST, colorButton, 335, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, colorButton, -105, SpringLayout.SOUTH, this);
+		setLayout(springLayout);
 	}
 	
 	private void setupListeners()
 	{
-		colorButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent mouseClick){changeBackgroundColor();}});//called here the color is changed on button press
+		colorButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent mouseClick){changeBackgroundColor();}});//the method changeBackgroundColor called here makes the color change on button press
 	}
 	
 	private void changeBackgroundColor() // this gets a random color and changes the label text
